eaf43f21b2d97ddad38bfed85de65d2ef69aad51
Dani JM <xdanielmtz@gmail.com>
2020-10-05 14:11:08 -0400

    buttons no longer refresh the page on click

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ var myCurrentDate = $("#currentDate").text(newM);
 // styling the date
 myCurrentDate.css("font-size", "20px");
 myCurrentDate.css("font-weight", "bold");
+
+// buttons no longer refresh the page upon click
+$( ".saveBtn" ).click(function( event ) {
+    event.preventDefault();
+  });
