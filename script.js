2dd9ead3448988bc877433fd5faf9cf5a6bcef35
Daniel Martinez <xdanielmtz@gmail.com>
2020-10-05 00:14:02 -0400

    styled the date

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var m = moment();
 var newM = m.format("dddd MMM Mo YYYY");
 
-$("#currentDate").text(newM);
+// sets current day bolow the title
+var myCurrentDate = $("#currentDate").text(newM);
+// styling the date
+myCurrentDate.css("font-size", "20px");
+myCurrentDate.css("font-weight", "bold");
