cceebcd8318a0e44d09caf15b4e3beb691e55577
Dani JM <xdanielmtz@gmail.com>
2020-10-05 23:14:06 -0400

    added useful comments in js. added the ability to obtain the current time

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 var m = moment();
+
+// shows the current date
 var newM = m.format("dddd MMM Mo YYYY");
+
+// shows the current time in 12hr format 
 var theCurrentTime = parseInt(moment().format("hh"));
 console.log(theCurrentTime);
 
 // sets current day below the title
 var myCurrentDate = $("#currentDate").text(newM);
+
 // styling the date
 myCurrentDate.css("font-size", "20px");
 myCurrentDate.css("font-weight", "bold");
+
+var hourNine = $("hour9")
+
+
+
+
+function comparingTime (){
+  if 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // buttons no longer refresh the page upon click
 $( ".saveBtn" ).click(function( event ) {
