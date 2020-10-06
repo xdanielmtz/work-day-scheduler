e6734613820f60436ab900cdde2ca9b830e5243f
Dani JM <xdanielmtz@gmail.com>
2020-10-06 03:07:56 -0400

    remove the data-hour atrribute from the div classes. added a mew class to target in the text area tags and gave it an individual id as well to compare the time. in the js i used a for each method to compare m current time with the id of the text area. the rows change color accordingly

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+$(document).ready(function() {
+
 var m = moment();
 
 // shows the current date
 var newM = m.format("dddd MMM Mo YYYY");
 
-// shows the current time in 12hr format 
+// shows the current time in 12hr format, assigned to a var.
 var theCurrentTime = parseInt(moment().format("hh"));
 console.log(theCurrentTime);
 
@@ -13,28 +15,37 @@ var myCurrentDate = $("#currentDate").text(newM);
 // styling the date
 myCurrentDate.css("font-size", "20px");
 myCurrentDate.css("font-weight", "bold");
+const rows = document.getElementsByClassName("newRows");
+let currentHour = parseInt(moment().format('H'));
 
-var hourNine = $("hour9")
+//used for each method to compare the current time with my id's of each text area tag in the html.
+Array.from(rows).forEach(textarea => {
+  let
+    theTaIdString = textarea.id,
+    theTaHour;
+  if (theTaIdString) {
+    theTaHour = parseInt(theTaIdString);
+  }
+  if (theTaHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === theTaHour) {
+      setColor(textarea, "red");
+    } else if ((currentHour < theTaHour) && (currentHour > theTaHour - 6)) {
+      setColor(textarea, "green");
+    } else if ((currentHour > theTaHour) && (currentHour < theTaHour + 6)) {
+      setColor(textarea, "lightgrey");
+    } else {
+      setColor(textarea, "lightgrey");
+    }
+  }
+});
 
-
-
-
-function comparingTime (){
-  if 
+//created this function to input the parameters of an element and color in the above section.
+function setColor(element, color) {
+  element.style.backgroundColor = color;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 // buttons no longer refresh the page upon click
 $( ".saveBtn" ).click(function( event ) {
